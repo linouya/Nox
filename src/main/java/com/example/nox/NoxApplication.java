7ce86b680091ca73ffd58ab3c9e93df00d17a378
ouyalin <linouya@hyperchain.cn>
2018-07-20 17:42:45 +0800

    feat: 初始化

---
+++ src/main/java/com/example/nox/NoxApplication.java
@@ -0,0 +1,12 @@
+package com.example.nox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoxApplication.class, args);
+    }
+}
